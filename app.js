c71dce234fac6d101d6c98807b5e2dea03d0eda6
Martin-no1tw <kljhger7728@gmail.com>
2021-11-05 22:54:16 +0800

    feat:add passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
 
 const app = express()
 const PORT = 3000
@@ -16,6 +17,12 @@ app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   return Todo.findAll({
