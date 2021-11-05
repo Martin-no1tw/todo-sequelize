b35cafd8e2295033a95d0a15024c7c01db598267
Martin-no1tw <kljhger7728@gmail.com>
2021-11-06 02:24:58 +0800

    add logout feature

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+
 const session = require('express-session')
 const usePassport = require('./config/passport')
-const passport = require('passport')
 
+const routes = require('./routes')
 const app = express()
 const PORT = 3000
 
 
-const db = require('./models')
-const todo = require('./models/todo')
-const Todo = db.Todo
-const User = db.User
 
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
@@ -25,78 +21,15 @@ app.use(session({
   saveUninitialized: true
 }))
 
+
+
 usePassport(app)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(routes)
 
 
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-  })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
 
 app.listen(PORT, () => {
   console.log(`APP is running on http://localhost:${PORT}`)
